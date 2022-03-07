3e2e6b4da101bd69a33013ab76b22716a2e52acc
Dillon Allan <dallan@ualberta.ca>
2022-03-06 21:11:33 -0700

    Add data models in RvmsModels project

---
+++ RvmsModels/Models.cs
@@ -0,0 +1,32 @@
+﻿namespace RvmsModels;
+
+using Newtonsoft.Json;
+
+public enum Role
+{
+    PATIENT,
+    ADMIN
+}
+
+public class User
+{
+    [JsonProperty(PropertyName = "email")] public string Id => Email;
+
+    [JsonProperty(PropertyName = "name")] public string Name { get; set; }
+    [JsonProperty(PropertyName = "email")] public string Email { get; set; }
+
+    [JsonProperty(PropertyName = "role")] public Role Role { get; set; }
+}
+
+public class Vitals
+{
+    [JsonProperty(PropertyName = "ECG")] public IEnumerable<TimestampedVital> Ecg { get; set; }
+    [JsonProperty(PropertyName = "HR")] public IEnumerable<TimestampedVital> HeartRate { get; set; }
+    [JsonProperty(PropertyName = "SpO2")] public IEnumerable<TimestampedVital> SpO2 { get; set; }
+}
+
+public struct TimestampedVital
+{
+    public DateTime Timestamp { get; set; }
+    public double Value { get; set; }
+}
